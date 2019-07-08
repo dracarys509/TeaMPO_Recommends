080c553222975391f1b6557d700dca3c3ccf969b
Nathan Braswell <nathan.braswell@gtri.gatech.edu>
2019-07-08 18:40:10 -0400

    really basic playing over google music

--- panel.py
+++ panel.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import sys
+import subprocess
+
+
 if len(sys.argv) < 2:
     print("please call with file to edit")
     sys.exit(1)
@@ -23,7 +26,12 @@ def center_pad(s, l):
     return (" "*left_pad_ammount) + s + (" "*right_pad_ammount)
 def right_pad(s, l):
     return s+" "*((l - len(s)))
-def to_string(keys, items):
+def to_string(keys, items, with_nums):
+    if with_nums:
+        keys = ["Number"] + keys
+        for i in range(len(items)):
+            items[i] = items[i].copy()
+            items[i]["Number"] = str(i)
     key_to_max_len = {}
     for key in keys:
         key_to_max_len[key] = max(len(key), max( len(item[key]) for item in items ) )
@@ -42,8 +50,12 @@ with open(filename) as f:
 (keys, data) = from_string(filestring)
 
 while True:
-    print(to_string(keys, data))
-    i = raw_input("What would you like to do? (q quit, a add Album (or song), w to write_out): ")
+    print(to_string(keys, data, True))
+    i = raw_input("What would you like to do? (q quit, a add Album (or song), l[number] to like, p[number] to play, w to write_out): ")
+    n = None
+    if len(i) > 1:
+        n = int(i[1:])
+        i = i[0]
     if i == "q":
         break;
     elif i == "a":
@@ -51,10 +63,27 @@ while True:
         for key in keys:
             item[key] = raw_input("value for " + key + ": ")
         data.append(item)
+    elif i == "l":
+        if n is None:
+            n = int(raw_input("number to like: ").strip())
+        current_count = int(data[n]["Likes"])
+        data[n]["Likes"] = str(current_count + 1)
+    elif i == "p":
+        if n is None:
+            n = int(raw_input("number to play: ").strip())
+        args = ["tizonia"]
+        if "(s)" in data[n]["Album"]:
+            args.append("--gmusic-unlimited-tracks")
+            args.append("".join(data[n]["Album"].split("(s)")) + " (" + data[n]["Artist"] + ")")
+        else:
+            args.append("--gmusic-unlimited-album")
+            args.append(data[n]["Album"] + " (" + data[n]["Artist"] + ")")
+        print("running " + " ".join(args) + "")
+        subprocess.call(args)
     elif i == "w":
         new_name = raw_input("filename to write (leave empty for previous filename " + filename + "): ")
         if new_name.strip() == "":
             new_name = filename
         with open(new_name, "w") as f:
-            f.write(to_string(keys, data))
+            f.write(to_string(keys, data, False))
 
